be8274ce4a5fcccb7b40b99761e691cf6407fedf
Michael Maier <maier1michael@gmail.com>
2018-03-09 16:40:46 +0100

    read secret backend_api_token

--- src/index.php
+++ src/index.php
@@ -1,2 +1,8 @@
 <?php
-phpinfo();
+if (!isset($_ENV['COMPOSER_VENDOR_DIR']) || !is_dir($_ENV['COMPOSER_VENDOR_DIR'])) {
+    exit('environment variable is not set: COMPOSER_VENDOR_DIR');
+}
+require_once $_ENV['COMPOSER_VENDOR_DIR'] . '/autoload.php';
+$dockerSecrets = new \DockerSecrets\Reader\SecretsReader();
+$backendApiToken = $dockerSecrets->read('backend_api_token');
+echo $backendApiToken . PHP_EOL;
